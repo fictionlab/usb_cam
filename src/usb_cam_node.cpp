89efcb1145776e096fe0d8ba98155e2633cdf4f9
Błażej Sowa <bsowa123@gmail.com>
2022-07-14 05:59:11 +0000

    Change nodelet name

--- src/usb_cam_node.cpp
+++ src/usb_cam_node.cpp
@@ -4,11 +4,11 @@
 int main(int argc, char **argv) {
   ros::init(argc, argv, "usb_cam");
 
-  // Start the ZED Nodelet
+  // Start the usb_cam Nodelet
   nodelet::Loader nodelet;
   nodelet::M_string remap(ros::names::getRemappings());
   nodelet::V_string nargv;
-  nodelet.load(ros::this_node::getName(), "usb_cam/UsbCamNodelet", remap, nargv);
+  nodelet.load(ros::this_node::getName(), "usb_cam/UsbCam", remap, nargv);
 
   ros::spin();
 
